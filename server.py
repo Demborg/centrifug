b7952beb6ed644735d43c51217ac9a0db2436bc7
Axel Demborg <axel.demborg@gmail.com>
2023-02-11 22:05:28 +0100

    add some text etc on the broadcast page

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from queue import Queue
 from typing import Sequence
 
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,11 +9,49 @@ CORS(app)
 
 listeners = [] # type: Sequence[Queue]
 
+descriptions = {
+    "01.mp4" : {
+        "title": "Genuary 01 - Perfect loop",
+        "medium": "Blender",
+        "text": ""
+    },
+    "02.mp4" : {
+        "title": "Genuary 02 - 10 minutes",
+        "medium": "Blender",
+        "text": ""
+    },
+    "03.mp4" : {
+        "title": "Genuary 03 - Glitch",
+        "medium": "Audacity, Spreadsheets",
+        "text": "A circular path in the frequency/Q-space of a notch filter applied to a self portrait."
+    },
+    "04.mp4" : {
+        "title": "Genuary 04 - Intersections",
+        "medium": "Blender",
+        "text": ""
+    },
+    "05.mp4" : {
+        "title": "Genuary 05 - Debug UI",
+        "medium": "Blender",
+        "text": ""
+    },
+    "06.mp4" : {
+        "title": "Genuary 06 - Steal like an artis",
+        "medium": "Blender",
+        "text": ""
+    },
+    "07.mp4" : {
+        "title": "Genuary 07 - Däggdjur",
+        "medium": "Python",
+        "text": ""
+    }
+}
+
 @app.route("/broadcast/<value>")
 def broadcast(value: str):
     for l in listeners:
         l.put_nowait(value)
-    return f"broadcasting value {value}"
+    return render_template("page.html", video=value, info=descriptions[value])
 
 @app.route("/listen")
 def listen():
